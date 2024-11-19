19bdde17baf1ee3c2e6ae74b10d4be8e6cd5ffcb
alejandro1223342 <alejomuoss@gmail.com>
2024-11-19 15:54:01 -0500

    Se realiza practica de let vs const

---
+++ REACNATIVE/Variables/variables.js
@@ -0,0 +1,20 @@
+let puntaje = 0;
+
+//Const no permite modificar
+//const numeroVidas = 5;
+
+let numeroVidas = 5;
+
+const iva = 0.12;
+
+ganarpuntos = () => {
+
+        puntaje = puntaje+10;
+}
+
+perderVidas = () =>{
+    //No se puede hacer
+    //numeroVidas=3;
+    numeroVidas= numeroVidas-1;
+
+}
