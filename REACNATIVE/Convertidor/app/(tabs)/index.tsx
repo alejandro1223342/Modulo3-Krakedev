8f1577e02562b67d40a18f43b944377c95b48ba4
alejandro1223342 <alejomuoss@gmail.com>
2024-11-22 15:45:41 -0500

    Se realiza practiva conversion de monedas

--- REACNATIVE/Convertidor/app/(tabs)/index.tsx
+++ REACNATIVE/Convertidor/app/(tabs)/index.tsx
@@ -1,74 +1,77 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import {  TextInput,StyleSheet, Text, View, Alert, Button } from 'react-native';
+//Repintar pantalla
+import {  } from 'react-native-gesture-handler';
+import{useState} from 'react'
+import { parse } from '@babel/core';
+export default function App() {
+const [valor1,setValor1] = useState("Ingrese un Valor en Dolares")
+  const [valorCambio,setValorCambio] = useState("");
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <Text>Ejemplo Convertidor</Text>
+      <Text>Ingresa un valor en Dólares</Text>
+      <Text>Valor del cambio {valorCambio}</Text>
+
+      <TextInput style={styles.cajaTexto}
+        value={valor1}
+
+        onChangeText={(txt) => {
+          setValor1(txt);
+        }}
+      />
+
+      <Button
+        title='PESOS COLOMBIANOS'
+
+        onPress={() => {
+          let resultado = parseFloat(valor1) * 4393.36
+          setValorCambio(resultado.toFixed(2).toString());
+        }}
+      />
+
+      <Button
+        title='PESOS MEXICANOS'
+
+        onPress={() => {
+          let resultado = parseFloat(valor1)*20.49
+          setValorCambio(resultado.toFixed(2).toString())
+
+        }}  
+      />
+
+      <Button 
+        title='EUROS'
+
+        onPress={() => {
+          let resultado= parseFloat(valor1)*0.96
+          setValorCambio(resultado.toFixed(2).toString())
+
+        }}    
+      />
+
+      
+
+      <StatusBar style="auto" />
+
+    </View>
   );
+
+
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
     alignItems: 'center',
-    gap: 8,
+    justifyContent: 'center',
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+  cajaTexto:{
+    borderColor:"black",
+    borderWidth:1,
+    paddingTop:5,
+    paddingHorizontal:10
+  }
 });
