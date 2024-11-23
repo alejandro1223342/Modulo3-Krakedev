d7df6e157c4ceb036bf5b437637eca429da6bafc
alejandro1223342 <alejomuoss@gmail.com>
2024-11-23 00:57:26 -0500

    Se realiza practica FlexBox

--- REACNATIVE/FlexBox/app/(tabs)/index.tsx
+++ REACNATIVE/FlexBox/app/(tabs)/index.tsx
@@ -7,10 +7,23 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Ejemplo Convertidor</Text>
 
     
-      <StatusBar style="auto" />
+    <View style={styles.contenedor2}></View>
+    <View style={styles.contenedor3}>
+
+      <View style={styles.contenedor4}></View>
+      <View style={styles.contenedor5}>
+        <View style={styles.contenedor6}></View>
+        <View style={styles.contenedor7}>
+          <Button title='BOTON 1'/>
+          <Button title='BOTON 2'/>
+          <Button title='BOTON 3'/>
+
+        </View>
+
+      </View>
+    </View>
 
     </View>
   );
@@ -21,8 +34,47 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'gray',
+    //Valor por defecto
+    flexDirection:'column'
   },
+
+  contenedor2:{
+    flex:1,
+    backgroundColor:'blue',
+
+  },
+
+  contenedor3:{
+    flex:3,
+    backgroundColor:'green',
+
+  },
+
+  contenedor4:{
+    flex:1,
+    backgroundColor:"yellow"
+
+  },
+
+  contenedor5:{
+    flex:1,
+    backgroundColor:"white",
+    flexDirection:"row"
+  },
+
+  contenedor6:{
+
+    flex:1,
+    backgroundColor:"pink"
+  },
+
+  contenedor7:{
+
+  flex:2,
+    backgroundColor:"purple",
+    flexDirection:'column',//Eje principal VERTICAL
+    justifyContent:'center',// CENTRADOS (principal)
+    alignItems:'stretch'//ocupa todo el espacio horizontal (secundario)
+  }
 });
