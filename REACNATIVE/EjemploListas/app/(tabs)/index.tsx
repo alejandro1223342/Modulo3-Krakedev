394a28b96f836ff5b89515e168920a9b29795269
alejandro1223342 <alejomuoss@gmail.com>
2024-11-25 17:40:54 -0500

    Se realiza practica de flatList y estilos de FlatList

--- REACNATIVE/EjemploListas/app/(tabs)/index.tsx
+++ REACNATIVE/EjemploListas/app/(tabs)/index.tsx
@@ -1,74 +1,83 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Alert, Button, FlatList } from 'react-native';
+//Repintar pantalla
+import { } from 'react-native-gesture-handler';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+let personas = [
+  { nombre: "Alejandro", apellido: "Muñoz", cedula: "1234567891" },
+  { nombre: "Paula", apellido: "Muñoz", cedula: "1234567892" },
+  { nombre: "Javier", apellido: "Muñoz", cedula: "1234567893" }
+];
 
-export default function HomeScreen() {
+export default function App() {
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <Text>PERSONAS</Text>
+      <FlatList style={styles.lista}
+
+        data={personas}
+        //Le paso un objeto
+        renderItem={(obj) => {
+
+          return (
+            <View style={styles.itemPersona}>
+              <Text style={styles.textoPrincipal}>{obj.index} {obj.item.nombre} {obj.item.apellido}</Text>
+              <Text style={styles.textoSecundario}>{obj.item.cedula}</Text>;
+            </View>);
+        }}
+
+        //Le paso un elemmento que no se repita
+        keyExtractor={(item)=>{
+          return item.cedula
+        }}
+
+      />
+      <StatusBar style="auto" />
+
+    </View>
   );
+
+
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    backgroundColor: 'lightblue',
+    flexDirection: 'column',//eje principal vertical
+    justifyContent:'flex-start',
+    alignItems:'stretch',
+    paddingTop: 50,
+    paddingHorizontal: 10
+
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+
+  lista:{
+    backgroundColor:'lightpink',
+
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+
+  itemPersona:{
+    backgroundColor:'lemonchiffon',
+    marginBottom:10,
+    paddingLeft:10
   },
+  cajaTexto: {
+    borderColor: "black",
+    borderWidth: 1,
+    paddingTop: 5,
+    padding: 10
+
+  },
+
+  textoPrincipal:{
+    fontSize:20
+
+  },
+
+  textoSecundario:{
+    fontStyle:'italic',
+    fontSize:16
+  }
 });
