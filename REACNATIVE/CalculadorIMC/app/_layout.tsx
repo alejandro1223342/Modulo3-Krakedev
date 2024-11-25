c46ae72f35df57ba43fdb73ee819eab0608d7e69
alejandro1223342 <alejomuoss@gmail.com>
2024-11-25 16:02:59 -0500

    Se realiza practica Calculdora IMC

--- REACNATIVE/CalculadorIMC/app/_layout.tsx
+++ REACNATIVE/CalculadorIMC/app/_layout.tsx
@@ -1,39 +1,89 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { useState } from 'react';
+import { TextInput, StyleSheet, Text, View, Alert, Button } from 'react-native';
+//Repintar pantalla
+import {  } from 'react-native-gesture-handler';
+export default function App() {
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
+  const [peso,setPeso]=useState("");
+  const [estatura,setEstatura]=useState("");
+  const [resultado,setResultado]=useState("");
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Ejemplo Convertidor</Text>
+
+      <TextInput style={styles.cajaTexto}
+        value={peso}
+        onChangeText={setPeso}
+        placeholder='Ingrese su Peso'
+      />
+
+      <TextInput
+        value={estatura} style={styles.cajaTexto}
+        onChangeText={setEstatura}
+        placeholder='Ingrese su Estatura en Centimetros'
+      />
+
+      <Button 
+        title='Calcular'
+        onPress={()=>{
+          let calculo;
+          calculo=parseFloat(peso)/((parseFloat(estatura) / 100) * (parseFloat(estatura) / 100));
+          setResultado(calculo.toFixed(2).toString());
+
+          if(calculo<18.5){
+            Alert.alert("Su peso es inferior al normal")
+          }else if(calculo>=18.5 && calculo<25.0){
+            Alert.alert("Su peso es normal")
+          }else if(calculo>=25.0 && calculo<30.0){
+            Alert.alert("Su peso es superior al normal")
+          }else if(calculo>=30){
+            Alert.alert("Tiene Obesidad")
+          }
+        }}
+
+      />
       <StatusBar style="auto" />
-    </ThemeProvider>
+
+      <Text  style={styles.subtitulo}>Su peso es {resultado}</Text>
+
+
+    </View>
   );
+
+
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    flexDirection:'column',//eje principal (vertical)
+    justifyContent:'center',//vertical
+    alignItems:'stretch',//horizontal
+    paddingHorizontal: 10
+  },
+
+  cajaTexto:{
+    borderColor:'gray',
+    borderWidth:1,
+    paddingTop:5,
+    paddingHorizontal:10,
+    marginBottom:10
+  
+  },
+
+  titulo:{
+    fontSize:30,
+    marginBottom:10,
+    fontStyle:'italic',
+        
+  },
+  
+  subtitulo:{
+    fontSize:15,
+    marginTop:20,
+    fontStyle:'italic',
+    justifyContent:'center'
+  }
+});
